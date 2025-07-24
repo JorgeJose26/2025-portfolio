43d59c67b6c92e813c74b8b32a13b590a3303ed3
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2025-07-24 11:39:31 -0400

    set up router, routes etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} />
+        </Routes>
+      </BrowserRouter>
+      <footer>
+        <p>© 2025 My Portfolio</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
